5249296b629771fbeaac2d76103360da365331a3
Fahriar Ahammed <fahriar.ahammed@outlook.com>
2022-09-08 18:09:11 +0600

    ....

--- app/Http/Controllers/ReportController.php
+++ app/Http/Controllers/ReportController.php
@@ -11,11 +11,12 @@ class ReportController extends Controller
 {
     public function index(Request $request)
     {
-        $attendance = Student::with(['midAttendanceCount', function ($query) use ($request) {
+        $attendance = Student::with(['midAttendanceCount' => function ($query) use ($request) {
             $query->where('course_name', $request->course_name)
                 ->where('attendance', 'p')
                 ->count();
         }])->get();
 
+        return response()->json($attendance);
     }
 }
